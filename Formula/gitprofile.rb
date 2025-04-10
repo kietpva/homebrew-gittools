44adb2f7f19216a168013ad0d94e6fb68d83990e
Kiet Pham <kiet.pham@asnet.com.vn>
2025-04-10 18:16:24 +0700

    Add formula for gitprofile v1.0.2

--- Formula/gitprofile.rb
+++ Formula/gitprofile.rb
@@ -2,7 +2,7 @@ class Gitprofile < Formula
   desc "Switch Git profiles easily with SSH and Git config"
   homepage "https://github.com/kietpva/git-profile"
   url "https://github.com/kietpva/git-profile/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "ab4f6a755b7e156d7382716468c9477047f21ff633837c3f508cb5baf7697703"
+  sha256 "9046127e873d17a14665660240930756d30e2f9f82a7a7a1e5fe29dbf51f69f6"
   version "1.0.2"
 
   def install
