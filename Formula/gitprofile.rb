f351578470d4232f2758353ae648640757aa0a21
Kiet Pham <kietpva0102@gmail.com>
2025-04-10 19:37:01 +0700

    update sha256

--- Formula/gitprofile.rb
+++ Formula/gitprofile.rb
@@ -2,7 +2,7 @@ class Gitprofile < Formula
   desc "Switch Git profiles easily with SSH and Git config"
   homepage "https://github.com/kietpva/git-profile"
   url "https://github.com/kietpva/git-profile/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "0798127e6ae296d7f4a030c0897841f2a12b1cfcf8cca4553976ab1758124c67"
+  sha256 "7f27388f3e1f26e96aa5b6f0f23c08672db6989806078145dc45eaf4e2a8a620"
   version "1.0.2"
 
   def install
