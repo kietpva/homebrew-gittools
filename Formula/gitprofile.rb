df4dbffddb20318faf1d3034f5fa202a1d144c13
Kiet Pham <kietpva0102@gmail.com>
2025-04-10 16:32:03 +0700

    setup

---
+++ Formula/gitprofile.rb
@@ -0,0 +1,11 @@
+class Gitprofile < Formula
+  desc "Switch Git profiles easily with SSH and Git config"
+  homepage "https://github.com/kietpva/git-profile"
+  url "https://github.com/kietpva/git-profile/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "a87e2a70c05900362221220bf5ec1a57cc3c5462f1e2dbd470c92d6819feb8bc"
+  version "1.0.0"
+
+  def install
+    bin.install "gitprofile.sh" => "gitprofile"
+  end
+end
